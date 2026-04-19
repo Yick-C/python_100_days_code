0f9b6c43fc70008cd276e3c3cd90feab2aa141a8
Christina <yickind@hotmail.co.uk>
2026-04-19 15:19:01 +0100

    Day 31 - flashcards

--- Day 31 - flash cards/main.py
+++ Day 31 - flash cards/main.py
@@ -6,15 +6,22 @@ import time
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_TITLE = ("Arial", 30, "italic")
 FONT_WORD = ("Arial", 40, "bold")
-
-data = pandas.read_csv("data/chinese_words_hsk3.csv")
-df_list = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/chinese_words_hsk3.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 # ---------------------------- READ LANGUAGE FILE ------------------------------- #
 def get_next_word():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
-    current_card = random.choice(df_list)
+    current_card = random.choice(to_learn)
     canvas.itemconfig(title_text, text="Chinese", fill="black")
     canvas.itemconfig(word_text, text=current_card['Chinese'], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
@@ -26,6 +33,12 @@ def flip_card():
     canvas.itemconfig(title_text, fill="white", text="English")
     canvas.itemconfig(word_text, fill="white", text=current_card['English'])
     canvas.itemconfig(pinyin_text, fill="white", text=current_card['Pinyin'])
+
+def is_known():
+    to_learn.remove(current_card)
+    learn_list = pandas.DataFrame(to_learn)
+    learn_list.to_csv("data/words_to_learn.csv", index=False)  # Don't add index to csv
+    get_next_word()
 
 # ---------------------------- FLASH CARD UI ------------------------------- #
 window = Tk()
@@ -45,13 +58,13 @@ canvas.grid(row=0, column=0, columnspan=2)
 
 
 # Buttons
-correct_btn_img = PhotoImage(file="images/right.png")
-correct_btn = Button(image=correct_btn_img, command=get_next_word)
-correct_btn.grid(row=1, column=1)
+known_btn_img = PhotoImage(file="images/right.png")
+known_btn = Button(image=known_btn_img, command=is_known)
+known_btn.grid(row=1, column=1)
 
-wrong_btn_img = PhotoImage(file="images/wrong.png")
-wrong_btn = Button(image=wrong_btn_img, command=get_next_word)
-wrong_btn.grid(row=1, column=0)
+unknown_btn_img = PhotoImage(file="images/wrong.png")
+unknown_btn = Button(image=unknown_btn_img, command=get_next_word)
+unknown_btn.grid(row=1, column=0)
 
 get_next_word()
 
