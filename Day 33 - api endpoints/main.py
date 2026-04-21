3bbaad96f4215b2a01a3a48f09d3427a69e92690
Christina <yickind@hotmail.co.uk>
2026-04-21 23:11:23 +0100

    Day 33 - apis and iss tracker

--- Day 33 - api endpoints/main.py
+++ Day 33 - api endpoints/main.py
@@ -1,14 +1,36 @@
 import requests
-url = "http://api.open-notify.org/iss-now.json"
+from datetime import datetime
 
-response = requests.get(url=url)
-response.raise_for_status()  # If not HTTP 200, raise exception
+URL = "http://api.open-notify.org/iss-now.json"
+SUNSET_URL = "https://api.sunrise-sunset.org/json"
+MY_LAT = 51.507351
+MY_LONG = -0.127758
 
-data = response.json()
-# print(data)
+# response = requests.get(url=URL)
+# response.raise_for_status()  # If not HTTP 200, raise exception
+#
+# data = response.json()
+# # print(data)
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+# iss_position = (longitude, latitude)
+#
+# print(iss_position)
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-iss_position = (longitude, latitude)
+# ==================================================================== #
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
 
-print(iss_position)
+res = requests.get(SUNSET_URL, params=parameters)
+res.raise_for_status()
+sunset_data = res.json()
+sunrise = sunset_data["results"]["sunrise"].split('T')[1].split(":")[0]  # Get hour from time
+sunset = sunset_data["results"]["sunset"].split('T')[1].split(":")[0]
+
+print(sunrise)
+
+time_now = datetime.now()
