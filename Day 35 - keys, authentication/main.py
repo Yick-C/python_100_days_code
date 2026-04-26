a595057f277594f0ea2ed6824e43765766378a63
Christina <yickind@hotmail.co.uk>
2026-04-26 22:27:16 +0100

    Day 35 - keys, authentication

--- Day 35 - keys, authentication/main.py
+++ Day 35 - keys, authentication/main.py
@@ -1,12 +1,13 @@
 import requests
-from twilio.rest import Client
+import os
+from twilio import *
 
 DAILY_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather"
 FIVE_DAY_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = ""
+api_key = os.environ.get("API_KEY")
 
-account_sid = ""
-auth_token = ""
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
 
 parameters = {
     "lat": "51.507351",
@@ -27,11 +28,10 @@ for weather in weather_data["list"]:
         will_rain = True
 
 if will_rain:
-    client = Client(account_sid, auth_token)
-    message = client.messages \
-        .create(
-        body="It's going to rain today. Bring an umbrella",
-        from="+12345678901",
-        to="+441234567890"
-    )
+    # client = Client(account_sid, auth_token)
+    # message = client.messages.create(
+    #     body="It's going to rain today. Bring an umbrella",
+    #     from_="+12345678901",
+    #     to="+441234567890",
+    # )
     print("Bring an umbrella.")
